d5430e7594ab9738a6dc53ec0655e00ce364cead
Jesús Iván Lemus Cervantes <thejesus567@gmail.com>
2025-05-09 01:59:34 -0600

    added public and private subnets

--- main.tf
+++ main.tf
@@ -3,10 +3,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr
 }
 
-output name {
-  value       = aws_vpc.myvpc.cidr_block
+resource "aws_subnet" "public-subnets" {
+  for_each          = var.public_subnet_cidr
+  cidr_block        = cidrsubnet(var.cidr, 8, each.value +1)
+  availability_zone = data.aws_availability_zones.available.names[each.value]
+  vpc_id            = aws_vpc.myvpc.id
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_subnet" "private-subnets" {
+  for_each = var.private_subnet_cidr
+  cidr_block        = cidrsubnet(var.cidr, 8, each.value + 4)
+  availability_zone = data.aws_availability_zones.available.names[each.value]
+  vpc_id            = aws_vpc.myvpc.id
+
+  tags = {
+    Name = each.key
+  }
+}
+
+output "az" {
+  value = data.aws_availability_zones.available.names
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,23 @@
-variable cidr {
+variable "cidr" {
   type        = string
   default     = "10.0.0.0/16"
   description = "CIDR block for the VPC. Defines the range of private IP addresses available for resources within the VPC"
+}
+
+variable "public_subnet_cidr" {
+  type = map(any)
+  default = {
+    public-subnet-1 : "0"
+    public-subnet-2 : "1"
+    public-subnet-3 : "2"
+  }
+}
+
+variable "private_subnet_cidr" {
+  type = map(any)
+  default = {
+    private-subnet-1 : "0"
+    private-subnet-2 : "1"
+    private-subnet-3 : "2"
+  }
 }
